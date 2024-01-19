7a8eec72a5abf951e14de9824f4abce5e34515d7
Abduvali1995 <johnhacker519@gmail.com>
2024-01-19 22:27:03 +0500

    urls va view qoshildi

--- app1/urls.py
+++ app1/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import first_view
+from .views import first_view,second_view,three_view,four_view
 
 urlpatterns =  [
     path('', first_view, name = 'first-view'),
+    path('second-view/', second_view, name='second-view'),
+    path('3-view/', three_view, name='second-view'),
+    path('4-view/', four_view, name='second-view')
 ]
--- app1/views.py
+++ app1/views.py
@@ -6,7 +6,28 @@ def first_view(requets):
     html = '''
         <h1> Assalamu alaykum bzining web sayitimizga hush kelibsiz </h1>
         <p>bugun juda qiziq narsa o`rganyapmiz</p>
-        <a href="https://www.google.com"> google </a>
+        <a href="second-view"> ikkinchi sahifa </a><br><br>
+        <a href="3-view"> 3 sahifa </a><br><br>
+        <a href="4-view"> 4 sahifa </a><br><br>
     '''
     return HttpResponse(html)
 
+def second_view(request):
+    html = '''
+        <h1>ikkinchi sahifa</h1>
+        <a href="../">orqaga</a>
+    '''
+    return HttpResponse(html)
+
+def three_view(request):
+    html = '''
+        <h1>3 sahifa</h1>
+        <a href="../">orqaga</a>
+    '''
+    return HttpResponse(html)
+def four_view(request):
+    html = '''
+        <h1>4 sahifa</h1>
+        <a href="../">orqaga</a>
+    '''
+    return HttpResponse(html)
